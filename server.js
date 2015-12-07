16c7699a02822d96a10a134d9205cf75430b549f
adityakadakia <kadakia@ccs.neu.edu>
2015-12-07 00:44:58 -0500

    server.js

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ require("./packages/system/server/app.js")(app, mongoose, db);
 
 
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
-var port      = process.env.OPENSHIFT_NODEJS_PORT || 3000;
+var port      = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 
 app.listen(port, ipaddress);
 
