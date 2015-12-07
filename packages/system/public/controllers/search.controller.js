0c7df19390b7976b35282bf0cd331ce102fc4c9b
Sahib <sahib.jaspal@gmail.com>
2015-12-06 23:39:35 -0500

    adding null check for username on details page also moving like check to success method

--- packages/system/public/controllers/search.controller.js
+++ packages/system/public/controllers/search.controller.js
@@ -31,7 +31,7 @@
             //choosing a random key
             while(true){
                 randomIndex = Math.floor(Math.random()*(maxLength+1)+0);
-                if(randomIndex<$rootScope.apiKeys.length){
+                if(randomIndex<maxLength){
                     break;
                 }
             }
